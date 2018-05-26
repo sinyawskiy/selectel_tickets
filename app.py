f446b6e784f910074f6e919ea41b3573b45639fb
dev13 <dev13@dev.dev>
2018-05-26 15:01:10 +0300

    add readme

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from config import Config
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+        # print(app.url_map)
         app.run()
     else:
         if sys.argv[1] == 'setup':
